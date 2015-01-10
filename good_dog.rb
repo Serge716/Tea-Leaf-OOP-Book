d388b0ba83debf54f7d495017134a87b22c0ce68
Sergino Delia <serge716@gmail.com>
2015-01-09 22:50:31 -0500

    exercises

--- good_dog.rb
+++ good_dog.rb
@@ -1,35 +1,75 @@
-module Speak
-  def speak(sound)
-    puts "#{sound}"
-  end
-end
+# module Speak
+#   def speak(sound)
+#     puts "#{sound}"
+#   end
+# end
 
-module Seat
-  def seat(msg)
-    puts "#{msg}"
-  end
-end
+# module Seat
+#   def seat(msg)
+#     puts "#{msg}"
+#   end
+# end
+
+# class GoodDog
+#   include Speak
+#   include Seat
+# end
+
+# class HumanBeing
+#   include Speak
+#   include Seat
+# end
+
+# sparky = GoodDog.new
+# sparky.speak("Arf!")
+# sparky.seat("Seating for a treat")
+
+# bob = HumanBeing.new
+# bob.speak("Hello!")
+# bob.seat("Taking a seat")
+
+# puts "____GoodDog Ancestors____"
+# puts GoodDog.ancestors
+# puts ''
+# puts "____HumanBeing Ancestors____"
+# puts HumanBeing.ancestors
+
+# class GoodDog
+#   def initialize
+#     puts "This object was initialized!"
+#   end
+# end
+
+# sparky = GoodDog.new
 
 class GoodDog
-  include Speak
-  include Seat
+
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  # def name
+  #   @name
+  # end
+
+  # def name=(n)
+  #   @name = n
+  # end
+
+  def speak
+    "#{@name} says arf!"
+  end
 end
 
-class HumanBeing
-  include Speak
-  include Seat
-end
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
+sparky.name = "Spartacus"
+sparky.name = "Spartacus"
+puts sparky.name
 
-sparky = GoodDog.new
-sparky.speak("Arf!")
-sparky.seat("Seating for a treat")
 
-bob = HumanBeing.new
-bob.speak("Hello!")
-bob.seat("Taking a seat")
-
-puts "____GoodDog Ancestors____"
-puts GoodDog.ancestors
-puts ''
-puts "____HumanBeing Ancestors____"
-puts HumanBeing.ancestors
+fido = GoodDog.new("Fido")
+puts fido.speak
